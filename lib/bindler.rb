76efc9fbc58d21b3be2d2303928887bcad844f45
Erran Carey <me@errancarey.com>
2013-07-23 20:45:16 -0500

    Move lib/vundler to lib/bindler

--- lib/bindler.rb
+++ lib/bindler.rb
@@ -1,6 +1,6 @@
-require_relative "vundler/version"
-require_relative "vundler/plugin"
-require_relative "vundler/logging"
-require_relative 'vundler/plugin_not_found_error'
-require_relative "vundler/local_plugins_manifest_ext"
-require_relative "vundler/bend_vagrant"
+require_relative "bindler/version"
+require_relative "bindler/plugin"
+require_relative "bindler/logging"
+require_relative 'bindler/plugin_not_found_error'
+require_relative "bindler/local_plugins_manifest_ext"
+require_relative "bindler/bend_vagrant"
